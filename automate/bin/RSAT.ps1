f2dafc50910240a00c877442c26d5f27680590c9
Chris <chris.gerke@gmail.com>
2017-03-02 06:48:24 +0800

    AD tools

--- automate/bin/RSAT.ps1
+++ automate/bin/RSAT.ps1
@@ -15,6 +15,13 @@ Switch -regex ($OperatingSystem)
     "Microsoft Windows 7 Enterprise" {
         %WINDIR%\system32\wusa.exe "$PSScriptRoot\Windows6.1-KB958830-x64-RefreshPkg.msu" /quiet /norestart /log:"C:\RSAT.log"
        dism /online /enable-feature /featurename:RemoteServerAdministrationTools
+       dism /online /enable-feature /featurename:RemoteServerAdministrationTools-Roles
+       dism /online /enable-feature /featurename:RemoteServerAdministrationTools-Roles-AD
+       dism /online /enable-feature /featurename:RemoteServerAdministrationTools-Roles-AD-DS
+       dism /online /enable-feature /featurename:RemoteServerAdministrationTools-Roles-AD-DS-Snapins
+       dism /online /enable-feature /featurename:RemoteServerAdministrationTools-Roles-AD-Powershell
+       dism /online /enable-feature /featurename:RemoteServerAdministrationTools-Roles-DHCP
+       dism /online /enable-feature /featurename:RemoteServerAdministrationTools-Roles-DNS
     }
     "Microsoft Windows Server 2008 R2 Enterprise" {
         Import-Module ServerManager
