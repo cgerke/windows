5b0a9459abfe95b6b0f8d5e80bacf6d730be6696
Chris Gerke <103310@corrsnet.au>
2019-04-03 14:06:23 +0800

    Build
    Using vscode build functionality to build ISOs

--- build.ps1
+++ build.ps1
@@ -1,1 +1,16 @@
 Write-Host "Hello world"
+
+function Invoke-CmdScript {
+    param([string]$script, [string]$parameters)
+    $tempFile = [IO.Path]::GetTempFileName()
+    cmd /c "`"$script`" `"$parameters`" && set > `"$tempFile`""
+    Get-Content $tempFile | Foreach-Object {
+        if($_ -match "^(.*?)=(.*)$"){
+            Set-Content "env:\$($matches[1])" $matches[2]
+        }
+    }
+    Remove-Item $tempFile
+}
+Write-Host "Setting envorionment variables for ADK..."
+Invoke-CmdScript -script "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\DandISetEnv.bat"
+Get-ChildItem env:\
