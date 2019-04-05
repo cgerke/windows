1eea739f9b65f2fc89dc0ddd583310392adcd7e4
Chris Gerke <103310@corrsnet.au>
2019-04-05 15:53:17 +0800

    Major
    - Complete builds
    - Filesystem browser

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
-# Verbose hack for input line is too long.
 param(
-   [switch] $Verbose
+    [ValidateNotNullOrEmpty()]
+    [System.IO.FileInfo]$File
 )
 
 function Invoke-Cmd {
@@ -19,55 +19,107 @@ function Invoke-Cmd {
     $tmpFile = [IO.Path]::GetTempFileName()
     cmd.exe /c "`"$File`" `"$parameters`" && set > `"$tmpFile`""
     Get-Content $tmpFile | ForEach-Object {
-        if($_ -match "^(.*?)=(.*)$"){
+        if ($_ -match "^(.*?)=(.*)$") {
             Set-Content "env:\$($matches[1])" $matches[2]
         }
     }
     Remove-Item $tmpFile
+
+    # Verbose ENV
+    Get-ChildItem env:\
+}
+
+function Invoke-FileBrowser {
+    Add-Type -AssemblyName System.Windows.Forms
+    $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
+        Multiselect = $false # Multiple files can be chosen
+        Filter      = 'Image (*.iso)|*.iso' # Specified file types
+    }
+
+    [void]$FileBrowser.ShowDialog()
+    $file = $FileBrowser.FileName;
+    If ($FileBrowser.FileNames -like "*\*") {
+        # Do something
+        $FileBrowser.FileName #Lists selected files (optional)
+    }
+    else {
+        Write-Host "Cancelled by user"
+    }
 }
 
 function Invoke-Extract {
     Param(
         [Parameter(Mandatory = $true)]
-        [string]$File
+        [ValidateScript( {
+                If (-Not ($_ | Test-Path) ) {
+                    Throw "File does not exist."
+                }
+                If (-Not ($_ | Test-Path -PathType Leaf) ) {
+                    Throw "The Path argument must be a file. Folder paths are not allowed."
+                }
+                If ($_ -notmatch "(\.iso)") {
+                    Throw "The file specified in the path argument must be an iso."
+                }
+                If (-Not (Get-DiskImage -ImagePath $_ | Get-Volume).DriveLetter ) {
+                    Mount-DiskImage -ImagePath $_ -StorageType ISO -PassThru
+                }
+                Return $true
+            })]
+        [System.IO.FileInfo]$File
     )
-    $7z = "C:\Program Files\7-Zip\7z.exe"
-    if ( Test-Path $7z ){
-        if ( Test-Path $File ){
-            $tmpPath = (Get-Item $File).Basename
-            Start-Process -FilePath $7z -ArgumentList "x -y -o""$tmpPath"" ""$File""" -NoNewWindow -Wait
-        } else {
-            Write-Output "! $(Get-PSCallStack) $File issue."
-            Exit
-        }
-    } else {
-        Write-Output "! $(Get-PSCallStack) Unable to extract $File without 7zip."
-        Exit
+
+    # install.wim test
+    $Drive = (Get-DiskImage -ImagePath $File | Get-Volume).DriveLetter
+    $Label = (Get-DiskImage -ImagePath $File | Get-Volume).FriendlyName
+    If (-Not "$Drive`:\sources\install.wim" | Test-Path ) {
+        Throw "Missing install.wim"
     }
 
+    # Working path
+    # $Build = [System.IO.Path]::GetRandomFileName()
+    $Build = [System.IO.Path]::GetFileNameWithoutExtension($File)
+
+    # Copy image contents
+    & Robocopy.exe "$drive`:\" ".\$Build\" /ETA /MIR /R:0 /W:0
+
+    # Customise
+    & imagex.exe /info ".\$Build\sources\boot.wim" >> ".\$Build\BUILD.txt"
+    & imagex.exe /info ".\$Build\sources\install.wim" >> ".\$Build\BUILD.txt"
+    & Robocopy.exe ".\automate" ".\$Build\automate" /ETA /MIR /R:1 /W:1
+
+    # TODO
+    # Invoke-FileBrowser as an option
+    Copy-Item ".\$Build.xml" ".\$Build\autounattend.xml"
+
+    $tools    = 'C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\oscdimg'
+    $oscdimg  = "$tools\oscdimg.exe"
+    $etfsboot = "$tools\etfsboot.com"
+    $efisys   = "$tools\efisys.bin"
+    $Arguments = '2#p0,e,b"{0}"#pEF,e,b"{1}"' -f $etfsboot, $efisys
+    Start-Process $oscdimg -args @("-bootdata:$Arguments", '-u2', '-udfver102', ".\$Build", ".\$Build.Unattended.iso") -wait -nonewwindow
+
+    # TODO
+    # Test for success then clean-up
+    # Remove-Item $workspace -recurse -force
+
+    # Clean up
+    # Dismount-DiskImage -ImagePath $File
 }
 
 # Invoke ADK
 Invoke-Cmd -File "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\DandISetEnv.bat"
-If ($Verbose) {
-    Get-ChildItem env:\
+
+# Build OS
+If ( $File ) {
+    # Get the full path to avoid issues with .\
+    $FilePath = Resolve-Path -Path $File
+    Invoke-Extract -File "$FilePath"
+}
+Else {
+    Invoke-Extract -File $(Invoke-FileBrowser)
 }
 
-# Extract ISO
-Invoke-Extract -File "$(Get-Location)\win7_pro_x64.ISO"
+# TODO
+# Build WinPE
 
-## provide a gui too ?
-<# Add-Type -AssemblyName System.Windows.Forms
-$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
-    Multiselect = $false # Multiple files can be chosen
-    Filter      = 'Images (*.iso, *.xml)|*.iso;*.xml' # Specified file types
-}
-
-[void]$FileBrowser.ShowDialog()
-$file = $FileBrowser.FileName;
-If ($FileBrowser.FileNames -like "*\*") {
-    # Do something
-    $FileBrowser.FileName #Lists selected files (optional)
-} else {
-    Write-Host "Cancelled by user"
-} #>
+# Build USB
